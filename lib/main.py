b36b35e5c45895625023d8d3aef58bb4a10aed98
Felix <neostyxde@gmail.com>
2020-10-28 21:07:43 +0100

    working!

--- lib/main.py
+++ lib/main.py
@@ -1,1 +1,37 @@
+import speech_recognition as speech
+from pynput.keyboard import Key, Controller
+import time
+import pydirectinput
 
+r = speech.Recognizer()
+keyboard = Controller()
+
+time.sleep(3)
+
+while 1:
+    time.sleep(0.3)
+
+    with speech.Microphone() as source:
+        audio_data = r.record(source, duration=5)
+        print("Recognizing...")
+        try:
+            text = r.recognize_google(audio_data, language = "de-DE")
+        except:
+            text = ""
+
+        print(text)
+
+    if text:
+        pydirectinput.keyDown('t')
+        time.sleep(0.1)
+        pydirectinput.keyUp('t')
+
+        time.sleep(0.2)
+
+        keyboard.type(text)
+
+        time.sleep(0.2)
+
+        pydirectinput.keyDown('enter')
+        time.sleep(0.1)
+        pydirectinput.keyUp('enter')
